89819b6e2ce6a49a0bf2848ca6f9a999847955ad
Aslan <nissanfordo03@gmail.com>
2023-11-09 21:04:01 +0600

    error_handling

---
+++ src/main/java/com/example/library_ms_project/controller/MyErrorController.java
@@ -0,0 +1,28 @@
+package com.example.library_ms_project.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "/errors/error_404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "/errors/error_500";
+            }
+        }
+        return "/errors/error";
+    }
+}
