e7fbe948b92a85c3583984f8c5172a5300dfb891
Aslan <90091576+sat0urn@users.noreply.github.com>
2024-06-24 13:55:57 +0500

    Delete target directory

--- target/classes/static/src/account.js
+++
@@ -1,8 +0,0 @@
-let image_input = document.getElementById("image_input");
-let image_submit_btn = document.getElementById("image_submit_btn");
-
-image_input.addEventListener('change', function(e) {
-    if (e.target.files[0]) {
-        image_submit_btn.classList.remove("d-none");
-    }
-});
